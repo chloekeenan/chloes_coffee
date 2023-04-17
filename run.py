d8484fd9badfe3a8898061bebd200470fbd45994
chloekeenan <chloe.keenan2001@gmail.com>
2023-04-17 09:23:37 +0000

    create get sales data function

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-# Your code goes here.
+
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
@@ -20,15 +20,14 @@ def jls_extract_def():
 
 
 def get_sales_data():
-    """
-    Get sales figures input from the user
-    """
+  
+    
     print("Please enter sales data from the last market.")
     print("Data should be six numbers, separated by commas.")
     print("Example: 10,20,30,40,50,60\n")
 
+data_str = input("Enter your data here: ")
+print(f"the data provided is {data_str} ")
 
-    data_str= input("Enter your data here:")
-    print(f"The data provided is {data_str}")
 
-    get_sales_data()
+get_sales_data()
